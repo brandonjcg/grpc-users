1f77904da062300c128b2f84d8aec7a1ffb9cb23
Brandon Castillo <brajoel98@gmail.com>
2025-03-03 21:21:18 -0800

    chore(notifications): :truck: definitions of clients moved to notification.module

--- notifications-microservice/src/app.module.ts
+++ notifications-microservice/src/app.module.ts
@@ -1,22 +1,7 @@
 import { Module } from '@nestjs/common';
-import { Transport, ClientsModule } from '@nestjs/microservices';
-import { join } from 'path';
 import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'NOTIFICATION_PACKAGE',
-        transport: Transport.GRPC,
-        options: {
-          url: 'localhost:50052',
-          package: 'notification',
-          protoPath: join(__dirname, '../../proto/notification.proto'),
-        },
-      },
-    ]),
-    NotificationModule,
-  ],
+  imports: [NotificationModule],
 })
 export class AppModule {}
--- notifications-microservice/src/notification/notification.module.ts
+++ notifications-microservice/src/notification/notification.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 
 @Module({
   controllers: [NotificationController],
   providers: [NotificationService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NOTIFICATION_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          url: 'localhost:50052',
+          package: 'notification',
+          protoPath: join(__dirname, '../../../proto/notification.proto'),
+        },
+      },
+    ]),
+  ],
 })
 export class NotificationModule {}
