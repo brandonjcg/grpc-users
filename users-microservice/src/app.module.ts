14fe03339eb2d0f7b3120a18fb9a0a5cd91960b1
Brandon Castillo <brajoel98@gmail.com>
2025-03-03 21:23:18 -0800

    chore(users): :fire: registration of client removed

--- users-microservice/src/app.module.ts
+++ users-microservice/src/app.module.ts
@@ -1,21 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { join } from 'path';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'USER_PACKAGE',
-        transport: Transport.GRPC,
-        options: {
-          package: 'user',
-          protoPath: join(__dirname, '../../proto/user.proto'),
-        },
-      },
-    ]),
-    UserModule,
-  ],
+  imports: [UserModule],
 })
 export class AppModule {}
